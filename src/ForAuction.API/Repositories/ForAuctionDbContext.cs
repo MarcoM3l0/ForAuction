60f195d2d3a571350fbe73574db12eee13af9446
Marco Melo <josemarcomelonascimento@outlook.com>
2024-02-06 00:39:13 -0300

    Adicionar arquivos de projeto.

---
+++ src/ForAuction.API/Repositories/ForAuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ForAuction.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForAuction.API.Repositories;
+
+public class ForAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source = C:\Users\User\Desktop\Dev\leilaoDbNLW.db");
+    }
+}
---
+++ src/ForAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,19 @@
+﻿using ForAuction.API.Entities;
+using ForAuction.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new ForAuctionDbContext();
+
+        var todey = DateTime.Now;
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => todey >= auction.Starts && todey <= auction.Ends);
+
+    }
+}
